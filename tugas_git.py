d0085d7c7e0dc886efb71392f01bbf41e0028db9
feizya25 <feizy.arizha@mhs.itenas.ac.id>
2024-12-05 17:35:24 +0700

    jawaban

--- tugas_git.py
+++ tugas_git.py
@@ -41,3 +41,60 @@ data_panen = {
     }
 }
 
+#1
+for lokasi, data in data_panen.items():
+    print(f"Lokasi: {data['nama_lokasi']}")
+    for komoditas, jumlah in data['hasil_panen'].items():
+        print(f"  {komoditas.capitalize()}: {jumlah} kg")
+    print()
+
+#2
+hasil_jagung_lok2 = data_panen["lokasi2"]["hasil_panen"]["jagung"]
+print(f"\nJumlah panen jagung dari lokasi2 (Kebun B): {hasil_jagung_lok2}")
+
+#3
+nama_lok3 = data_panen["lokasi3"]["nama_lokasi"]
+print(f"Nama lokasi dari lokasi3: {nama_lok3}")
+
+#4
+hasil_padi = {}
+hasil_kedelai = {}
+for lokasi, data in data_panen.items():
+    hasil_padi[lokasi] = data['hasil_panen']['padi']
+    hasil_kedelai[lokasi] = data['hasil_panen']['kedelai']
+
+print("\nHasil panen padi per lokasi:", hasil_padi)
+print("Hasil panen kedelai per lokasi:", hasil_kedelai)
+
+#5
+hasil_padi_lok1 = data_panen["lokasi1"]["hasil_panen"]["padi"]
+hasil_kedelai_lok1 = data_panen["lokasi1"]["hasil_panen"]["kedelai"]
+
+hasil_padi_lok2 = data_panen["lokasi2"]["hasil_panen"]["padi"]
+hasil_kedelai_lok2 = data_panen["lokasi2"]["hasil_panen"]["kedelai"]
+
+hasil_padi_lok3 = data_panen["lokasi3"]["hasil_panen"]["padi"]
+hasil_kedelai_lok3 = data_panen["lokasi3"]["hasil_panen"]["kedelai"]
+
+hasil_padi_lok4 = data_panen["lokasi4"]["hasil_panen"]["padi"]
+hasil_kedelai_lok4 = data_panen["lokasi4"]["hasil_panen"]["kedelai"]
+
+hasil_padi_lok5 = data_panen["lokasi5"]["hasil_panen"]["padi"]
+hasil_kedelai_lok5 = data_panen["lokasi5"]["hasil_panen"]["kedelai"]
+
+print(f"\nHasil panen padi lokasi1: {hasil_padi_lok1}, kedelai: {hasil_kedelai_lok1}")
+print(f"\nHasil panen padi lokasi2: {hasil_padi_lok2}, kedelai: {hasil_kedelai_lok2}")
+print(f"\nHasil panen padi lokasi3: {hasil_padi_lok3}, kedelai: {hasil_kedelai_lok3}")
+print(f"\nHasil panen padi lokasi4: {hasil_padi_lok4}, kedelai: {hasil_kedelai_lok4}")
+print(f"\nHasil panen padi lokasi5: {hasil_padi_lok5}, kedelai: {hasil_kedelai_lok5}")
+
+#6
+print("\nStatus dari Lokasi:")
+for lokasi, data in data_panen.items():
+    padi = data['hasil_panen']['padi']
+    jagung = data['hasil_panen']['jagung']
+    if padi > 1300 or jagung > 800:
+        print(f"{data['nama_lokasi']} memerlukan perhatian khusus.")
+    else:
+        print(f"{data['nama_lokasi']} dalam kondisi baik.")
+
